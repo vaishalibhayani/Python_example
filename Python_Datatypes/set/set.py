2e58018e553eb3ef329600568e993500c5fd6c71
vaishalibhayani <59078625+vaishalibhayani@users.noreply.github.com>
2021-10-30 23:21:19 +0200

    Add files via upload

---
+++ Python_Datatypes/set/set.py
@@ -0,0 +1,20 @@
+#define empty set
+
+# emp=set() #bulid in function
+
+# print(emp)
+# print(type(emp))
+
+emp2={"vaishali","dhaval","sunil","snehal",333444}
+print(emp2,type(emp2))
+
+# add data in set
+emp2.add("kusum")
+
+print(emp2)
+
+# remove data in set
+
+emp2.remove("dhaval")
+
+print(emp2)
